ffc54ac91aa72b90ece8838eaa4dcab1bb3f2a9f
mmorganj41 <mmorganj41@gmail.com>
2023-02-16 11:50:00 -0500

    add post_gcd function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
     let server = HttpServer::new(|| {
@@ -25,4 +32,21 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the gCD with zero is boring.");
+    }
+
+    let response = 
+        format!("The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
